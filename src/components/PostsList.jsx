c5877faafd5286434b83ab523554b51e06b0ab5b
setayesh hoseiini <setayesh.hoseiini79@gmail.com>
2023-07-16 16:41:27 +0330

    show dialog and close when click behind it

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -2,24 +2,39 @@ import Post from "./Post";
 import classes from './PostsList.module.css'
 import NewPost from './NewPost';
 import { useState } from "react";
+import Modal from "./Modal";
+
 
 function PostsLists() {
+  const [modalIsVisible, setModalIsVisible] = useState(true);
   const [enteredBody, setEnteredBody] = useState('')
-  const [enterAuthor , setEnteredAuthor] = useState('');
+  const [enterAuthor, setEnteredAuthor] = useState('');
 
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
 
   function bodyChangeHandler(event) {
     setEnteredBody(event.target.value);
   }
 
-  function authoreChangeHandler(event){
+  function authoreChangeHandler(event) {
     setEnteredAuthor(event.target.value);
 
   }
 
   return (
     <>
-      <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authoreChangeHandler} />
+      {
+        modalIsVisible
+          ? (<Modal onClose={hideModalHandler}>
+            <NewPost
+              onBodyChange={bodyChangeHandler}
+              onAuthorChange={authoreChangeHandler}
+            />
+          </Modal>
+         ) : false}
+
       <ul className={classes.posts}>
         <Post author={enterAuthor} body={enteredBody} />
         <Post author="manual" body="Check out the full course!" />
